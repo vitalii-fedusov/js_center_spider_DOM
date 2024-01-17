f111dd43c147fe4a12f67caefbb9b3f4290e69cc
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-17 10:21:43 +0200

    make build

---
+++ index.a1460325.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=t.clientWidth;t.style.left="".concat(c/2-l/2,"px"),t.style.top="".concat(c/2-l/2,"px");
+//# sourceMappingURL=index.a1460325.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const spiderWidth = spider.clientWidth;
+spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+spider.style.top = `${wallWidth / 2 - spiderWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
